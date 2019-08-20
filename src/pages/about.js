f7168425ab258ac0a32441d7bbb307a29116cb77
Franklin Castillo <franklincastillo18@gmail.com>
2019-08-20 13:50:16 -0400

    New Layout component to manage header and footer easily

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,17 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 const About = () => {
   return (
     <div>
-      <Header />
-      <h1>About me</h1>
-      <h2>Im a sofware developer play around with Gatsby</h2>
-      <p>
-        <Link to="/contact">Go to contact page</Link>
-      </p>
-      <Footer />
+      <Layout>
+        <h1>About me</h1>
+        <h2>Im a sofware developer play around with Gatsby</h2>
+        <p>
+          <Link to="/contact">Go to contact page</Link>
+        </p>
+      </Layout>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,21 @@
 import React from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 const Contact = () => {
   return (
     <div>
-      <Header />
-      <h1>Contact Page</h1>
-      <h2>
-        Contact me in Twitter:
-        {/* _blanc open a new tab when clicking the link */}
-        <a href="https://twitter.com/franklincr11" target="_blank">
-          @franklincr11
-        </a>
-      </h2>
-      <Footer />
+      <Layout>
+        <h1>Contact Page</h1>
+        <h2>
+          Contact me in Twitter:
+          {/* _blanc open a new tab when clicking the link */}
+          <a href="https://twitter.com/franklincr11" target="_blank">
+            @franklincr11
+          </a>
+        </h2>
+      </Layout>
     </div>
   )
 }
