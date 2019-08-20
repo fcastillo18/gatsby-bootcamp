a29df260e2004be9606eed0240051e8d5a75b8a7
Franklin Castillo <franklincastillo18@gmail.com>
2019-08-20 13:15:32 -0400

    New about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <h2>This will be the about page</h2>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <h2>This will be the conctact page</h2>
+    </div>
+  )
+}
+
+export default Contact
