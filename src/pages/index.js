31c904a64fa69354a35ea560e289988ff596798b
Franklin Castillo <franklincastillo18@gmail.com>
2019-08-20 13:06:40 -0400

    Hello World with gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I'm Franklin, full-stack dev living in the Dominican Republic</h2>
+    </div>
+  )
+}
+
+export default IndexPage
