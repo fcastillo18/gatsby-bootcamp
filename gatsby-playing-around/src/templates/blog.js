e20f89cf4dd7c4757e87ec885a5c3ba060ba6c33
Franklin Castillo <franklincastillo18@gmail.com>
2019-08-23 21:45:02 -0400

    renderering post data in blog template

--- gatsby-playing-around/src/templates/blog.js
+++ gatsby-playing-around/src/templates/blog.js
@@ -1,10 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = props => {
   return (
     <div>
-      <Layout>This is the blog template</Layout>
+      <Layout>
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        <p>{props.data.markdownRemark.frontmatter.date}</p>
+        <div
+          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+        ></div>
+      </Layout>
     </div>
   )
 }
